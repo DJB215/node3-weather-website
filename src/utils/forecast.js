b42c302c4fd237defba1d261a0d138517fefa96c
Darrell Bullock <darrellbullock@Darrells-MacBook-Air.local>
2020-09-23 16:26:24 -0400

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, lon, callback) => {
             callback('Unable to find the location. Please try another search.', undefined)
         } else {
             callback(undefined, 
-            body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike)
+            body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + '. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
